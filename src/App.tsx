181fc9c696eacfbba0e5233ef1f728e574a8d369
Oleksandra Makar <oleksandra.makar@dataart.com>
2023-01-03 22:50:35 +0100

    Added packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { Container, Typography } from '@mui/material'
+import AddToDo from './components/AddToDo'
+import ToDoList from './components/ToDoList'
+import CompletedToDosCounter from './components/CompletedToDosCounter'
 
 function App() {
-    return <div>react query</div>
+    return (
+        <Container maxWidth="xs">
+            <Typography style={{ textAlign: 'center' }} variant="h3">
+                Redux List App
+            </Typography>
+            <AddToDo />
+            <ToDoList />
+            <CompletedToDosCounter />
+        </Container>
+    )
 }
 
 export default App
